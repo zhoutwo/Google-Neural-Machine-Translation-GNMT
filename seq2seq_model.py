1d0cb8709692be2d0ff723de651222d9187e7249
shawnxu1318 <shawnxu1318@gmail.com>
2017-03-03 01:21:00 -0800

    bug fixed

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -1,18 +1,3 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 """Sequence-to-sequence model with an attention mechanism."""
 
 from __future__ import absolute_import
@@ -119,11 +104,11 @@ class Seq2SeqModel(object):
     for layer in xrange(num_layers):
       if layer % 2 == 0:
         with tf.device('/gpu:0'):
-          single_cell = tf.nn.rnn_cell.LSTMCell(size)
+          single_cell = tf.nn.rnn_cell.LSTMCell(size/2)
         list_of_cell.append(single_cell)
       else:
         with tf.device('/gpu:1'):
-          single_cell = tf.nn.rnn_cell.LSTMCell(size)
+          single_cell = tf.nn.rnn_cell.LSTMCell(size/2)
         list_of_cell.append(single_cell)
 
     if num_layers > 1:
