04718e2ae5aefc5048a36cf7fb448fb1c4afdedc
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-03-25 11:57:56 -0400

    Fix argument order

--- translate.py
+++ translate.py
@@ -186,7 +186,7 @@ def train():
         # Create model.
         print("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
         model = create_model(sess, False)
-        model = create_or_load_model(model, sess, True)
+        model = create_or_load_model(sess, model, True)
 
         # Read data into buckets and compute their sizes.
         print("Reading development and training data (limit: %d)."
