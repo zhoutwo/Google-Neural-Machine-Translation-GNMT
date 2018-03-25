01d1c10cab9285630eb48ac22cae70a75f184d30
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-03-25 00:20:24 -0400

    Disable default graph when saving dis_model

--- translate.py
+++ translate.py
@@ -416,7 +416,7 @@ def train():
                 _reset_tf_graph_random_seed()
 
                 save_checkpoint(train_sess, train_model)
-                dis_model.save(os.path.join(FLAGS.train_dir, str(train_model.global_step.eval(session=train_sess)) + '.h5'))
+            dis_model.save(os.path.join(FLAGS.train_dir, str(train_model.global_step.eval(session=train_sess)) + '.h5'))
             # Update eval_model with new weights
             with tf.device("/cpu:0"):
                 eval_model = create_or_load_model(eval_sess, eval_model)
