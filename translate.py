95705e02547fd72c53f6fce36bc695911ff5aec7
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-05-07 01:20:25 -0400

    Avoid infinite loop by using a for loop

--- translate.py
+++ translate.py
@@ -298,13 +298,12 @@ def decode():
 
             print("Reshaping model inputs")
             batch_encoder_inputs, batch_decoder_inputs, batch_target_weights = [], [], []
-            i = 0
-            while i < len(inputs):
+            for i in range(len(inputs)):
                 if i + 64 < len(inputs):
                     next_size = 64
                 else:
                     next_size = len(inputs) - i
-                i += 1
+
                 next_batch_encoder_inputs = input_encoder_inputs[i:i+next_size]
                 next_batch_decoder_inputs = input_decoder_inputs[i:i+next_size]
                 next_batch_target_weights = input_target_weights[i:i+next_size]
