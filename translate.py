9fd894b8d8d178f46e0b9981215f3b89c96841ff
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-05-06 23:40:15 -0400

    Attempt to make batching myself

--- translate.py
+++ translate.py
@@ -279,41 +279,60 @@ def decode():
         _, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
 
         if FLAGS.decode_input:
-            with open(FLAGS.decode_input, "r") as input_file:
-                with open(FLAGS.decode_input + "_actual", "w") as actual_input_file:
-                    with open(FLAGS.decode_output, "w") as output_file:
-                        line = input_file.readline()
-                        while line:
-                            try:
-                                # Get token-ids for the input sentence.
-                                token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(line), en_vocab)
-                                # Which bucket does it belong to?
-                                bucket_id = min([b for b in range(len(_buckets))
-                                                 if _buckets[b][0] >= len(token_ids)])
-                                # Get a 1-element batch to feed the sentence to the model.
-                                encoder_inputs, decoder_inputs, target_weights = model.get_batch(
-                                    {bucket_id: [(token_ids, [])]}, bucket_id)
-                                # Get output logits for the sentence.
-                                print(model.batch_size)
-                                assert model.batch_size != 1
-                                _, _, output_logits_arr = model.step(sess, encoder_inputs, decoder_inputs,
-                                                                 target_weights, bucket_id, True)
-                                # This is a greedy decoder - outputs are just argmaxes of output_logits.
-                                print(output_logits_arr)
-                                print(len(output_logits_arr))
-                                print(output_logits_arr[0])
-                                print(len(output_logits_arr[0]))
-                                print(output_logits_arr[0].shape)
-                                outputs = [[int(np.argmax(logit, axis=1)) for logit in output_logits] for output_logits in output_logits_arr]
-                                # If there is an EOS symbol in outputs, cut them at that point.
-                                outputs = [output[:output.index(data_utils.EOS_ID)] if data_utils.EOS_ID in output else output for output in outputs]
-                                for output_sentence in outputs:
-                                    output_file.write((" ".join([tf.compat.as_str(rev_fr_vocab[output_word]) for output_word in output_sentence])) + "\n")
-                                actual_input_file.write(line)
-                            except Exception as e:
-                                print("Error occurred while decoding", line, ", and the error was:", str(e))
-                                raise e
-                            line = input_file.readline()
+            inputs = [line.strip() for line in open(FLAGS.decode_input, "r")]
+            input_token_ids = [data_utils.sentence_to_token_ids(tf.compat.as_bytes(line), en_vocab) for line in inputs]
+            input_bucket_ids = [min([b for b in range(len(_buckets)) if _buckets[b][0] >= len(token_ids)]) for token_ids in input_token_ids]
+            max_bucket_id = max(input_bucket_ids)
+            input_encoder_inputs, input_decoder_inputs, input_target_weights = [], [], []
+            for i in range(len(inputs)):
+                encoder_inputs, decoder_inputs, target_weights = model.get_batch({max_bucket_id: [(input_token_ids[i], [])]}, max_bucket_id)
+                input_encoder_inputs.append([a[0] for a in encoder_inputs])
+                input_decoder_inputs.append([a[0] for a in decoder_inputs])
+                input_target_weights.append([a[0] for a in target_weights])
+
+            batch_encoder_inputs, batch_decoder_inputs, batch_target_weights = [], [], []
+            i = 0
+            while i < len(inputs):
+                if i + 64 < len(inputs):
+                    next_size = 64
+                else:
+                    next_size = len(inputs) - i
+                next_batch_encoder_inputs = input_encoder_inputs[i:i+next_size]
+                next_batch_decoder_inputs = input_decoder_inputs[i:i+next_size]
+                next_batch_target_weights = input_target_weights[i:i+next_size]
+
+                e_in_len = len(next_batch_encoder_inputs[0])
+                d_in_len = len(next_batch_decoder_inputs[0])
+                t_in_len = len(next_batch_target_weights[0])
+
+                reshaped_next_batch_encoder_inputs = np.array([[a[j] for a in next_batch_encoder_inputs] for j in range(e_in_len)], dtype=np.int32)
+                reshaped_next_batch_decoder_inputs = np.array([[a[j] for a in next_batch_decoder_inputs] for j in range(d_in_len)], dtype=np.int32)
+                reshaped_next_batch_target_weights = np.array([[a[j] for a in next_batch_target_weights] for j in range(t_in_len)], dtype=np.int32)
+
+                batch_encoder_inputs.append(reshaped_next_batch_encoder_inputs)
+                batch_decoder_inputs.append(reshaped_next_batch_decoder_inputs)
+                batch_target_weights.append(reshaped_next_batch_target_weights)
+
+            with open(FLAGS.decode_output, "w") as output_file:
+                for i in range(len(batch_encoder_inputs)):
+                    encoder_inputs, decoder_inputs, target_weights = batch_encoder_inputs[i], batch_decoder_inputs[i], batch_target_weights[i]
+                    try:
+                        _, _, output_logits_arr = model.step(sess, encoder_inputs, decoder_inputs,
+                                                         target_weights, max_bucket_id, True)
+                        # This is a greedy decoder - outputs are just argmaxes of output_logits.
+                        print(output_logits_arr)
+                        print(len(output_logits_arr))
+                        print(output_logits_arr[0])
+                        print(len(output_logits_arr[0]))
+                        print(output_logits_arr[0].shape)
+                        outputs = [[int(np.argmax(logit, axis=1)) for logit in output_logits] for output_logits in output_logits_arr]
+                        # If there is an EOS symbol in outputs, cut them at that point.
+                        outputs = [output[:output.index(data_utils.EOS_ID)] if data_utils.EOS_ID in output else output for output in outputs]
+                        for output_sentence in outputs:
+                            output_file.write((" ".join([tf.compat.as_str(rev_fr_vocab[output_word]) for output_word in output_sentence])) + "\n")
+                    except Exception as e:
+                        print("Error occurred while decoding", [" ".join([tf.compat.as_str(rev_fr_vocab[output_word] for output_word in encoder_input) for encoder_input in encoder_inputs], ", and the error was:", str(e))
+                        raise e
 
         else:
             model.batch_size = 1  # We decode one sentence at a time.
